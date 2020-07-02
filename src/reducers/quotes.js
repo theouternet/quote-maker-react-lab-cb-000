d82e58dd159c0cff26cfbca34f2bb92e169c0b92
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-02 03:08:43 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
 export default (state = [], action) => {
-  return state;
+   let idx;
+  let selectedQuote;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [].concat(state, action.quote);
+    case "REMOVE_QUOTE":
+      idx = state.map(quote => quote.id).indexOf(action.quoteId);
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length));
+    case "UPVOTE_QUOTE":
+      idx = state.map(quote => quote.id).indexOf(action.quoteId);
+      selectedQuote = state[idx];
+      selectedQuote.votes++;
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length)).concat(selectedQuote);
+    case "DOWNVOTE_QUOTE":
+      idx = state.map(quote => quote.id).indexOf(action.quoteId);
+      selectedQuote = state[idx];
+      if (selectedQuote.votes > 0 ) {
+          selectedQuote.votes--;
+      }
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length)).concat(selectedQuote);
+    default:
+      return state;
+  };
 }
